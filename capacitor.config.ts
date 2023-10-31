fe3be0e7f7dbe1358aaabaa7fcdbefc86875b057
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-10-31 08:53:56 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'stencil-warning',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
